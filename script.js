c8febff94491898d5db38218d37cf91c61ef57b7
Connor Vosberg <connorjv@gmail.com>
2022-10-24 10:04:31 -0700

    updated fav surfers list

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+/* const favSurfers = document.createElement("h2");
+favSurfers.textContent = "Favorite Surfers";
+
+document.querySelector("body").appendChild(favSurfers);
+
+const surfersList = document.createElement("p")
+surfersList.textContent = ["Mikey February","John John Florence"]
+
+document.querySelector("body").appendChild(surfersList);
+
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Favorite Surf Spots";
 
 document.querySelector("body").appendChild(h2);
+
+const p = document.createElement("p")
+p.textContent = ["Bolsa Chica","Huntington Beach","Malibu","San Onofre","Las Saladitas"]
+document.querySelector("body").appendChild(p); */
